f61c584849fbc99911c3ba2f241ba450d9bae50f
Pratyush Dhakal <pratyshdhakal.personal@gmail.com>
2025-02-13 10:50:40 +0545

    add searchModal

---
+++ search.js
@@ -0,0 +1,11 @@
+export let isSearchModalOpen = false;
+
+export function closeSearchModal() {
+	isSearchModalOpen = false;
+}
+
+export function openSearchModal() {
+	isSearchModalOpen = true;
+}
+
+
